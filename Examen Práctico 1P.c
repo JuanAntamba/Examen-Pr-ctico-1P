1c52e6f415c35b71b659ebe7b89158f257a70d1b
Juan Antamba <juan.antamba@udla.edu.ec>
2023-10-24 15:14:59 -0500

    Se agrega la estructura for , para la repetcion del ciclo necesario tomando en cuenta n

--- Examen Práctico 1P.c
+++ Examen Práctico 1P.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int main() {
@@ -10,6 +11,11 @@ int main() {
 
     if (n < 1) {
         printf("El valor de n debe ser mayor o igual a 1.\n"); // Se crea un if para la restrccion de i
+    } else {
+        for (int i = 1; i <= n; i++) {  // estructura for para que se repita la operacion requerida, tomando en cuanta n veces
+            suma += 1.0 / (double)(1 << i);  // Sumar 1/2 elevado a i a la suma, se cambio a double por el tamaño de los decimales
+        }
     }
 }
+
 
