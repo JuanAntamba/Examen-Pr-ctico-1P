539556e7d591b5051417cd2bab2a342cd51c8921
Juan Antamba <juan.antamba@udla.edu.ec>
2023-10-24 15:11:47 -0500

    Se crea la estrucutra repetitiva if para las restricciones

--- Examen Práctico 1P.c
+++ Examen Práctico 1P.c
@@ -7,5 +7,9 @@ int main() {
 
     printf("Ingrese un valor para n: ");// se pide al usuario que ingrese el numero n y se leearan 
     scanf("%d", &n);
+
+    if (n < 1) {
+        printf("El valor de n debe ser mayor o igual a 1.\n"); // Se crea un if para la restrccion de i
+    }
 }
 
